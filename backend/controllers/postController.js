582d6593e44792354a4cfa0e87ddf552f7b87d28
huzaifa560 <huzaifanaveedx@gmail.com>
2026-08-09 14:53:56 +0500

    Update postController.js

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import Post from "../models/Post.js";
+import Post from "../models/post.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const uploadsDir = path.join(__dirname, "..", "uploads");
